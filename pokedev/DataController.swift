7ccdd6a08e5b56fdf139e3a5e4d480704db857b8
Deiby Toralva <deiby@devacademy.la>
2016-04-17 17:26:07 -0500

    Delete data controller

--- pokedev/DataController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DataController.swift
-//  pokedev
-//
-//  Created by Deiby Toralva Baldeón on 17/04/16.
-//  Copyright © 2016 Deiby Toralva Baldeón. All rights reserved.
-//
-
-import Foundation
