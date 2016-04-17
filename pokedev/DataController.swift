674278c97f99e78d37acf5bf341a2831a221b8b7
Deiby Toralva <deiby@devacademy.la>
2016-04-17 17:25:47 -0500

    Add data controller

---
+++ pokedev/DataController.swift
@@ -0,0 +1,9 @@
+//
+//  DataController.swift
+//  pokedev
+//
+//  Created by Deiby Toralva Baldeón on 17/04/16.
+//  Copyright © 2016 Deiby Toralva Baldeón. All rights reserved.
+//
+
+import Foundation
